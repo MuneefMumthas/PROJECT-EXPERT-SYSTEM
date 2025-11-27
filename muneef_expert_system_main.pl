b4c3c73df3f90c8a951b4a10468d19aa51e59ab3
MuneefMumthas <22206529@bucks.ac.uk>
2025-11-27 10:17:08 +0000

    Addde intel cpus as knowledge base for computer hardware expert system

---
+++ muneef_expert_system_main.pl
@@ -0,0 +1,17 @@
+% Knowledge base for the computer hardware expert system
+
+
+% cpu(Tier, Brand, Model, Socket, Price)
+
+% lga1700 socket cpus
+cpu(high, intel, i9_14900KS, lga1700, 630).
+cpu(medium, intel, i7_14700K, lga1700, 420).
+cpu(low, intel, i3_12100F, lga1700, 90).
+
+% lga1200 socket cpus
+cpu(high, intel, i9_11900K, lga1200, 350).
+cpu(medium, intel, i5_11600K, lga1200, 175).
+cpu(low, intel, i3_10100F, lga1200, 75).
+
+
+
