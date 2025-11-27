e5611204eca3a2ab5769977b9d13c39778ac2ae2
MuneefMumthas <22206529@bucks.ac.uk>
2025-11-27 11:20:19 +0000

    added am5 and am4 mother boards

--- muneef_expert_system_main.pl
+++ muneef_expert_system_main.pl
@@ -27,7 +27,7 @@ cpu(low, amd, ryzen_3_4100, am4, 60).
 
 
 % motherboard(Tier, Brand, Model, Socket, Ram type, Price)
-% lga1700 motherboards
+% lga1700 motherboards - they use both ddr4 and ddr5 ram
 % ddr4 and lga1700 motherboards
 motherboard(high, asus, tuf_gaming_b760_plus_wifi_d4, ddr4, lga1700, 180).
 motherboard(medium, asus, prime_b760m_a_wifi_d4, ddr4, lga1700, 130).
@@ -38,12 +38,23 @@ motherboard(high, gigabyte, z790_aorus_master, ddr5, lga1700, 580).
 motherboard(medium, asus, rog_strix_b760f_gaming_wifi, ddr5, lga1700, 250).
 motherboard(low, msi, b760_gaming_plus_wifi, ddr5, lga1700, 130).
 
-% lga1200 motherboards
+% lga1200 motherboards - they all use ddr4 ram
 % ddr4 and lga1200 motherboards
 motherboard(high, asus, rog_maximus_xiii_extreme, ddr4, lga1200, 840).
 motherboard(medium, msi, mpg_z490_gaming_plus, ddr4, lga1200, 380).
 motherboard(low, gigabyte, h410m_h_v2, ddr4, lga1200, 50).
 
+% am5 motherboards - they all use ddr5 ram
+% ddr5 and am5 motherboards
+motherboard(high, asus, rog_crosshair_x870e_apex, ddr5, am5, 675).
+motherboard(medium, asus, rog_strix_b650e_f_gaming_wifi, ddr5, am5, 180).
+motherboard(low, msi, pro_a620m_e, ddr5, am5, 70).
+
+% am4 motherboards - they all use ddr4 ram
+% ddr4 and am4 motherboards
+motherboard(high, asus, rog_strix_b550_f_gaming, ddr4, am4, 155).
+motherboard(medium, gigabyte, b550m_ds3h, ddr4, am4, 85).
+motherboard(low, msi, a520m_a_pro, ddr4, am4, 50).
 
 
 
