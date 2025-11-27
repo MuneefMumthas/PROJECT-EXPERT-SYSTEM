aaaf38ad19ef0f1009faa967deac10dab822df2d
MuneefMumthas <22206529@bucks.ac.uk>
2025-11-27 10:38:26 +0000

    Add AMD CPUs to the knowledge base

--- muneef_expert_system_main.pl
+++ muneef_expert_system_main.pl
@@ -3,6 +3,7 @@
 
 % cpu(Tier, Brand, Model, Socket, Price)
 
+%intel cpus
 % lga1700 socket cpus
 cpu(high, intel, i9_14900KS, lga1700, 630).
 cpu(medium, intel, i7_14700K, lga1700, 420).
@@ -12,6 +13,17 @@ cpu(low, intel, i3_12100F, lga1700, 90).
 cpu(high, intel, i9_11900K, lga1200, 350).
 cpu(medium, intel, i5_11600K, lga1200, 175).
 cpu(low, intel, i3_10100F, lga1200, 75).
+
+%amd cpus
+% AM5 socket cpus
+cpu(high, amd, ryzen_9_9950x, am5, 660).
+cpu(medium, amd, ryzen_7_7700x, am5, 360).
+cpu(low, amd, ryzen_5_7600, am5, 220).
+
+% AM4 socket cpus
+cpu(high, amd, ryzen_9_5950X, am4, 500).
+cpu(medium, amd, ryzen_5_5600X, am4, 260).
+cpu(low, amd, ryzen_3_4100, am4, 60).
 
 
 
