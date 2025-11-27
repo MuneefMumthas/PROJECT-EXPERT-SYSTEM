b8e36815e8881b58b5500d42c8aa0968b46c6513
MuneefMumthas <22206529@bucks.ac.uk>
2025-11-27 10:57:33 +0000

    added lga1700 motherboards

--- muneef_expert_system_main.pl
+++ muneef_expert_system_main.pl
@@ -26,4 +26,18 @@ cpu(medium, amd, ryzen_5_5600X, am4, 260).
 cpu(low, amd, ryzen_3_4100, am4, 60).
 
 
+% motherboard(Tier, Brand, Model, Socket, Ram type, Price)
+% ddr4 and lga1700 motherboards
+motherboard(high, asus, tuf_gaming_b760_plus_wifi_d4, ddr4, lga1700, 180).
+motherboard(medium, asus, prime_b760m_a_wifi_d4, ddr4, lga1700, 130).
+motherboard(low, msi, pro_h610m_g_ddr4, ddr4, lga1700, 60).
+
+% ddr5 and lga1700 motherboards
+motherboard(high, gigabyte, z790_aorus_master, ddr5, lga1700, 580).
+motherboard(medium, asus, rog_strix_b760f_gaming_wifi, ddr5, lga1700, 250).
+motherboard(low, msi, b760_gaming_plus_wifi, ddr5, lga1700, 130).
+
+
+
+
 
